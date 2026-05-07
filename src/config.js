da741a5cd40ef6e0ef4db5cb770feef1c97d263c
fr4nc1s0017 <fr4nc1s0017@gmail.com>
2026-05-07 08:14:43 -0600

    url

--- src/config.js
+++ src/config.js
@@ -2,17 +2,9 @@ import{config} from 'dotenv'
 config()
 
 export const PORT= process.env.PORT || 3000
-export const DB_HOST=process.env.DB_HOST || 'localhost'
+export const DB_HOST=process.env.DB_HOST || 'Mysql.mysql.railway.internal'
 export const DB_PORT=process.env.DB_PORT || 3306
 export const DB_USER=process.env.DB_USER || 'root'
-export const DB_PASSWORD=process.env.DB_PASSWORD || ''
-export const DB_DATABASE=process.env.DB_DATABASE || 'market'
-MYSQL_DATABASE="railway"
-MYSQL_PUBLIC_URL="mysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_TCP_PROXY_DOMAIN}}:${{RAILWAY_TCP_PROXY_PORT}}/${{MYSQL_DATABASE}}"
-MYSQL_ROOT_PASSWORD="cOPklyYyCVowBKTviCmwBpQFtIVESmnR"
-MYSQL_URL="mysql://${{MYSQLUSER}}:${{MYSQL_ROOT_PASSWORD}}@${{RAILWAY_PRIVATE_DOMAIN}}:3306/${{MYSQL_DATABASE}}"
-MYSQLDATABASE="${{MYSQL_DATABASE}}"
-MYSQLHOST="${{RAILWAY_PRIVATE_DOMAIN}}"
-MYSQLPASSWORD="${{MYSQL_ROOT_PASSWORD}}"
-MYSQLPORT="3306"
-MYSQLUSER="root"
+export const DB_PASSWORD=process.env.DB_PASSWORD || 'cOPklyYyCVowBKTviCmwBpQFtIVESmnR'
+export const DB_DATABASE=process.env.DB_DATABASE || 'railway'
+export const DB_URL=process.env.DB_URL || 'mysql://root:cOPklyYyCVowBKTviCmwBpQFtIVESmnR@mysql.railway.internal:3306/railway'
